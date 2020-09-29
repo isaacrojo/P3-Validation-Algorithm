8c04d325aa52666d2464218679dcb4c786999d8e
isaacrojo <60981406+isaacrojo@users.noreply.github.com>
2020-09-28 19:48:11 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Validation_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int uno = 1;
+
+            Console.WriteLine("Inserte cualquier número para comenzar algoritmo");
+            int numTyped = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numTyped; i++)
+            {
+                System.Console.Write(uno + ", ");
+            }
+            System.Console.Write(numTyped);        
+        }
+    }
+}
